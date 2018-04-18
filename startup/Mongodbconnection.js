57b0a0a7d66449eb8c22c1a0d3ae5200f0a9c4ae
gaber <mohamed95Gaber@gmail.com>
2018-04-18 17:04:56 +0200

    cloudedb

--- startup/Mongodbconnection.js
+++ startup/Mongodbconnection.js
@@ -1,9 +1,10 @@
 const mongoose=require('mongoose');
 
-let dbvar='mongodb://localhost:27017/firstproject_db';
+let dbvar='mongodb://localhost:27017/zoo-management';
 
 if(process.env.NODE_ENV === "production"){
-    dbvar="dah elconnection string from server";
+    console.log("anahena");
+    dbvar="mongodb://admin:admin@ds149309.mlab.com:49309/zoo-management";
 }
 
 let conn=mongoose.connect(dbvar,(err)=>{
