bce147ee1362fcec44c3142f02909d136ee4d03e
gaber <mohamed95Gaber@gmail.com>
2018-04-19 14:35:36 +0200

    add delete route

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/add', function(req, res, next) {
-  debugger;
   user.create(req.body, function (err, inserteddata) {
     if (err){return handleError(err);}
     res.json(inserteddata);
@@ -24,12 +23,32 @@ router.post('/add', function(req, res, next) {
 
  });
 
-//  router.get('/delete', function(req, res, next) {
-//   user.delete({},function(err,data){
-//     res.json(data);
+ router.delete('/delete/:_id', function(req, res, next) {
+   console.log(req.params._id);
+  user.deleteOne({ "_id":req.params._id }, function (err,data) {
+    if(!err) return res.json(data);
+    else{
+      console.log(err);
+    }
 
-//   });
- 
+  });
+
+  // user.remove({_id: req.params._id }).exec( function (err,data) {
+  //   if (err) {
+  //     res.send(err);
+  //   }
+  //   res.send(data);
+  //   // removed!
+  // });
+  // user.findByIdAndRemove(req.params._id, function(err,data) {
+  //   if (data) {
+  //           res.json({"data":data})
+  //   }
+  //   else {
+  //           res.send(err);
+  //   }
+  // });
+});
 
 module.exports = router;
 
